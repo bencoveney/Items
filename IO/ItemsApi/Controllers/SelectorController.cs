07951337b9638b59df2137325ba4dfdee0a91278
bencoveney <bencoveney@gmail.com>
2015-10-08 20:17:37 +0100

    Fixed dependency conflicts

--- IO/ItemsApi/Controllers/SelectorController.cs
+++ IO/ItemsApi/Controllers/SelectorController.cs
@@ -1,24 +1,24 @@
 ﻿namespace ItemsApi.Controllers
 {
-    using Items;
-    using ItemsApi;
-    using ItemsApi.Models;
-    using System.Collections.Generic;
-    using System.Web.Http;
-    using System.Linq;
-    using System.Net.Http;
+	using Items;
+	using ItemsApi;
+	using ItemsApi.Models;
+	using System.Collections.Generic;
+	using System.Web.Http;
+	using System.Linq;
+	using System.Net.Http;
 
-    [RoutePrefix("api/selectors")]
-    public class ItemSelectorController : ApiController
-    {
-        [Route("data")]
-        [HttpPost]
-        public IHttpActionResult GetDataPost([FromBody]ItemSelector selection)
-        {
-            if (selection == null)
-                return BadRequest("selection must be provided");
+	[RoutePrefix("api/selectors")]
+	public class ItemSelectorController : ApiController
+	{
+		[Route("data")]
+		[HttpPost]
+		public IHttpActionResult GetDataPost([FromBody]ItemSelector selection)
+		{
+			if (selection == null)
+				return BadRequest("selection must be provided");
 
-            return Ok("Bello!");
-        }
-    }
+			return Ok("Bello!");
+		}
+	}
 }
